3cc97c70b67f38a28f89b4b019da96d6ded3941a
Emir Ali <emirhassan.ali02@gmail.com>
2022-09-19 14:16:34 -0300

    fix: Erros with keyboard layout

--- lib/app/providers/login_provider.dart
+++ lib/app/providers/login_provider.dart
@@ -60,7 +60,7 @@ class LoginProvider extends ChangeNotifier {
         signIn = true;
         _sharedPref.setBool(isLoggedInString, true);
         _sharedPref.setString("language", "es");
-        _sharedPref.setString('keyboardLayout', 'Qwerty');
+        _sharedPref.setString('keyboardLayout', 'QWERTY');
         debugPrint('yes');
       }
     } catch (e) {
